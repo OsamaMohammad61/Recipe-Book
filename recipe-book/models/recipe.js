4ba066d27a74020e8963b963444de78b466a79be
OsamaMohammad61 <osamamohammad343@gmail.com>
2024-05-26 16:48:19 +0300

    merged the cuisines with the other pages

--- recipe-book/models/recipe.js
+++ recipe-book/models/recipe.js
@@ -1,27 +1,19 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
-const Schema = mongoose.Schema;
+const Schema = mongoose.Schema
 
+const recipeSchema = new Schema(
+  {
+    cuisine: { type: String, required: true },
+    Dish: { type: String, required: true },
+    Time: { type: Number, required: true },
+    Ingredents: { type: String, required: true },
+    Steps: { type: String, required: true },
+    doneBY: [{ type: Schema.Types.ObjectId, ref: 'User' }]
+  },
+  {
+    timestamps: true
+  }
+)
 
-
-const recipeSchema = new Schema({
-cuisine: {type: String,
-    required: true
-},
-Dish: {type: String,
-    required: true
-},
-Time: {type: Number,
-    required: true
-},
-Ingredents: {type: String,
-    required: true
-},
-Steps: {type: String,
-    required: true
-}
-}, {
-timestamps: true
-});
-
-module.exports = mongoose.model('Recipe', recipeSchema);
+module.exports = mongoose.model('Recipe', recipeSchema)
--- recipe-book/routes/recipes.js
+++ recipe-book/routes/recipes.js
@@ -1,15 +1,14 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
 
-const recipesCtrl = require('../controllers/recipes');
-	
-router.get('/recipe', recipesCtrl.index);
+const recipesCtrl = require('../controllers/recipes')
 
-router.get('/new', recipesCtrl.new);
-router.post('/recipes/addRecipe', recipesCtrl.addRecipe);
+router.get('/recipe', recipesCtrl.index)
 
-//router.get('/:id', recipesCtrl.show);
+router.get('/new', recipesCtrl.new)
+router.post('/recipes/addRecipe', recipesCtrl.addRecipe)
 
+//router.get('/allrecipes', recipesCtrl.showAll)
+router.get('/cuisines', recipesCtrl.showCuisines)
 
-	
-module.exports = router;
+module.exports = router
