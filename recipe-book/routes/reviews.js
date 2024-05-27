dc450f2f93f2b260ea2ff773945f051704d4d0c6
OsamaMohammad61 <osamamohammad343@gmail.com>
2024-05-27 09:50:11 +0300

    changed the recipe and review schema

--- recipe-book/routes/reviews.js
+++ recipe-book/routes/reviews.js
@@ -1,9 +1,9 @@
 const express = require('express')
-const router = express.Router();
+const router = express.Router()
 const reviewCtrl = require('../controllers/reviews')
 
 router.get('/recipe/review', reviewCtrl.reviewForm)
-router.post('/recipe/addReview', reviewCtrl.addReview)
+router.post('/recipe/:id/addReview', reviewCtrl.addReview)
 router.get('/recipes/allreviews', reviewCtrl.allReviews)
 
 module.exports = router
