5ec9fbbdde68fb13b16267c49ace756d423841ef
OsamaMohammad61 <osamamohammad343@gmail.com>
2024-05-29 09:06:37 +0300

    working on the edit review function

--- recipe-book/routes/reviews.js
+++ recipe-book/routes/reviews.js
@@ -6,8 +6,8 @@ router.get('/recipe/review', reviewCtrl.reviewForm)
 router.post('/recipe/:id/addReview', reviewCtrl.addReview)
 router.get('/recipes/allreviews', reviewCtrl.allReviews)
 router.post('/review/:id', reviewCtrl.Onereview)
-router.post('/update/:id/review', reviewCtrl.update)
 router.get('/edit/:id', reviewCtrl.edit)
+router.put('/update/:id/review', reviewCtrl.update)
 router.delete('/deleteit/:id', reviewCtrl.delete)
 
 module.exports = router
